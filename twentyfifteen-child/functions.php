6786eb75f44960062309efb3e4d3d8a3a836f6b8
geomarts <netdark@hotmail.gr>
2018-12-17 20:30:30 +0200

    Updating functions.php

--- twentyfifteen-child/functions.php
+++ twentyfifteen-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 function my_theme_enqueue_scripts() {
-  $parent_style = 'twentyfifteen-style';  
+  $parent_style = 'twentyfifteen-style';
 
   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
   wp_enqueue_style( 'child-style',
@@ -8,8 +8,8 @@ function my_theme_enqueue_scripts() {
     array( $parent_style ),
     wp_get_theme()->get('Version')
   );
-  
-  wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/assets/js/main.js', array(), false, true );
+
+  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array(), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_scripts' );
 ?>
